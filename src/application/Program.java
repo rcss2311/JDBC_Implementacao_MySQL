3220c347cf25bf065c6dc42d269ca7ad733d2053
Ricardo Souza <rcss2311@gmail.com>
2020-12-01 17:36:59 -0300

    Transações setAutocommit, commit, rollback

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,8 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 import db.DbExceptions;
@@ -12,26 +12,37 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Connection conn = null;
-		PreparedStatement st = null;
+		Statement st = null;
 		
 		try {
 			conn = DB.getConnection();
 			
-			st = conn.prepareStatement(
-					"DELETE FROM department "
-					+ "WHERE "
-					+"Id = ? "
-			);
+			conn.setAutoCommit(false);// toda transação estará sujeita a confirmação
 			
-			st.setInt(1, 2);
+			st = conn.createStatement();
 			
-			int rowsAffected = st.executeUpdate();
+			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
 			
-			System.out.println("Done! Rows Afectted: "+ rowsAffected);
+			/*int x =  1;
+			if (x < 2) {
+				throw new SQLException("Fake Error!");
+			}*/
 			
+			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+			
+			conn.commit();//executando a confimação da transação
+			
+			System.out.println("Rows 1: "+rows1);
+			System.out.println("Rows 2: "+rows2);
 		}
 		catch(SQLException e) {
-			throw new DbExceptions(e.getMessage());
+			try {
+				conn.rollback();
+				throw new DbExceptions("Transaction rolled back! Caused By: "+e.getMessage());
+			} 
+			catch (SQLException e1) {
+				throw new DbExceptions("Error trying to rollback! Caused by: "+ e1.getMessage() ); 
+			}
 		}
 		finally {
 			DB.closeStatement(st);
